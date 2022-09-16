3eaa06094978c465bacc5b37db52d6fc5df4ad5b
Federico Sanchez <105878085+fefosan40@users.noreply.github.com>
2022-09-16 11:09:45 -0300

    arreglos routing container details

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import{BrowserRouter,Routes,Route} from "react-router-dom"
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/Productos/ItemListContainer"
 import ItemDetailsContainer from"./components/Details/ItemDetailsContainer"
 import Cart from "./components/Cart";
 import CartProvider from "./components/Context/CartContext";
 
 
+
 function App() {
   return (
   <CartProvider>
   <BrowserRouter>
-  <Navbar/>
+  <NavBar/>
   <Routes>
           <Route exact path="/" element={<ItemListContainer/>} />
-            <Route exact  path="/category/:categoryId" element={<ItemListContainer />} />
             <Route exact  path="/category/:categoryId" element={<ItemListContainer />} />
            <Route exact path="/item/:id" element={<ItemDetailsContainer/>} />
            <Route  path="/cart" element={<Cart/>} />
